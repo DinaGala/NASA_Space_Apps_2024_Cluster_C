cc1165527c57bea974f63bdb31aab4a7790e0d93
Nadezhda Zhuzhleva <dina.zhuzhleva@gmail.com>
2024-10-06 10:53:30 +0200

    prepared a version for uri

--- dev/requirements/django/satellite_tracker/tracking/views.py
+++ dev/requirements/django/satellite_tracker/tracking/views.py
@@ -31,6 +31,32 @@ wrs2_shapefile = os.path.join(settings.BASE_DIR, 'data/WRS2_descending.shp')
 # Чтение shapefile в GeoDataFrame
 wrs2_gdf = gpd.read_file(wrs2_shapefile)
 
+def get_new_image_time(path, row):
+        try:
+            start_date = '2024-01-01'
+            end_date = '2024-12-31'
+
+            landsat_collection_8 = ee.ImageCollection('LANDSAT/LC08/C02/T1').filter(ee.Filter.eq('WRS_PATH', int(path))).filter(ee.Filter.eq('WRS_ROW', int(row))).filter(ee.Filter.date(start_date, end_date))
+            last_image_8 = landsat_collection_8.sort('system:time_start', False).first()
+
+            if last_image_8:
+                timestamp = last_image_8.get('system:time_start').getInfo()
+                timestamp_seconds = timestamp / 1000
+                last_image_datetime_8 = datetime.utcfromtimestamp(timestamp_seconds)
+
+            landsat_collection_9 = ee.ImageCollection('LANDSAT/LC09/C02/T1').filter(ee.Filter.eq('WRS_PATH', int(path))).filter(ee.Filter.eq('WRS_ROW', int(row))).filter(ee.Filter.date(start_date, end_date))
+            last_image_9 = landsat_collection_9.sort('system:time_start', False).first()
+            if last_image_9:
+                timestamp = last_image_9.get('system:time_start').getInfo()
+                timestamp_seconds = timestamp / 1000
+                last_image_datetime_9 = datetime.utcfromtimestamp(timestamp_seconds)
+                
+            last_image_datetime = min(last_image_datetime_8, last_image_datetime_9)
+            next_image = last_image_datetime + timedelta(days=16)
+                return next_image
+
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
 
 
 class LandsatLastImageView(View):
@@ -111,47 +137,23 @@ def get_wrs2_path_row(latitude, longitude):
     return None, None
 
 
-# Function to convert UTC timestamp to local time
-def convert_utc_to_local(utc_time, local_timezone):
-    utc_time = datetime.utcfromtimestamp(utc_time)
-    local_time = utc_time.replace(tzinfo=pytz.utc).astimezone(local_timezone)
-    return local_time.strftime('%Y-%m-%d %H:%M:%S')
-
-
-# Function to get timezone from latitude and longitude
-def get_timezone_from_coords(latitude, longitude):
+# Function to get timezone from latitude and longitude and convert UTC timestamp to local time
+def get_timezone_from_coords(next_image_utc, latitude, longitude):
     tf = TimezoneFinder()
     timezone_str = tf.timezone_at(lat=latitude, lng=longitude)
     if timezone_str:
-        return pytz.timezone(timezone_str)
+        local_timezone = pytz.timezone(timezone_str)
     else:
         # Fallback to UTC if no timezone found
-        return pytz.utc
+        local_timezone = pytz.utc
+    next_image_local = next_image_utc.replace(tzinfo=pytz.utc).astimezone(local_timezone)
+    return next_image_local
 
-def get_landsat_passes(request):
-    # Get latitude and longitude from frontend (GET or POST request)
-    latitude = float(request.GET.get('latitude'))
-    longitude = float(request.GET.get('longitude'))
-    altitude = float(request.GET.get('altitude', 100))  # Default altitude
+def postpone(user_email, next_image_time_utc)
+# HERE GOES CELERY
+                # Отправка уведомления
+                # send_notification_email.apply_async(args=[user_email], eta=next_image)  # next_image - время отправки
 
-    # Get the local timezone for the given coordinates
-    local_timezone = get_timezone_from_coords(latitude, longitude)
+                # Отправка датасета через 12 часов
+                # download_and_send_dataset.apply_async(args=[user_email], eta=next_image + timedelta(hours=24))
 
-    # Get passes for Landsat 8 and Landsat 9 using provided coordinates
-    landsat_8_passes = get_satellite_passes(LANDSAT_8_NORAD_ID, latitude, longitude, altitude)
-
-    # Extract the start and end times in local tntntgime for both satellites
-    result = {
-        'landsat_8': []
-    }
-    
-    for p in landsat_8_passes:
-        result['landsat_8'].append({
-            'start_time': convert_utc_to_local(p['startUTC'], local_timezone),
-            'end_time': convert_utc_to_local(p['endUTC'], local_timezone),
-        })
-    
-    
-    
-    # Return the times as JSON response
-    return JsonResponse(result)
